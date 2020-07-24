e1f1a173a92ceabcc8fa8814dfce8da42e9506b6
jyunpeimamiya <jyunpei0225@yahoo.com>
2020-07-25 00:34:58 +0900

    tweak

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'messages/index'
-  root "messages#index"
-  resources :users, only: [:edit, :update]
+
+ 
+  root "users#index"
+  resources :users, only: [:index, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
