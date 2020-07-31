e5657cf7fcda4b41baee0b1d27f62ad0a024e490
jyunpeimamiya <jyunpei0225@yahoo.com>
2020-07-31 16:23:20 +0900

    practice-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
-  def edit
+  def index
   end
+  # def edit
 
-  def update
-    if current_user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-    private
+  # end
 
-    def user_params
-      params.require(:user).parmit(:name, :email)
-    end
+  # def update
+  #   if current_user.update(user_params)
+  #     redirect_to root_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #   private
+
+  #   def user_params
+  #     params.require(:user).parmit(:name, :email)
+  #   end
   end
